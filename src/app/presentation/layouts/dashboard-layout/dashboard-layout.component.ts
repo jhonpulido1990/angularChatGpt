a73d0d698918f5b8c9cd0436a38b17f29aea83e4
jhonpulido1990 <3825@holbertonstudents.com>
2025-01-16 17:44:09 -0500

    menu lateral

--- src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidebarMenuItemComponent } from '../../components/sidebar-menu-item/sidebar-menu-item.component';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SidebarMenuItemComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardLayoutComponent {}
+export class DashboardLayoutComponent {
+  public routes = routes[0].children?.filter((route) => route.data)
+}
