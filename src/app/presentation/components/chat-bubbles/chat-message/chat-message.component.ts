164131915a38a8eee0446b671d0867bfc946b6f2
jhonpulido1990 <3825@holbertonstudents.com>
2025-01-30 17:14:19 -0500

    angular mardown

--- src/app/presentation/components/chat-bubbles/chat-message/chat-message.component.ts
+++ src/app/presentation/components/chat-bubbles/chat-message/chat-message.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-chat-message',
   standalone: true,
-  imports: [],
+  imports: [
+    MarkdownModule
+  ],
   templateUrl: './chat-message.component.html',
-  styleUrl: './chat-message.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatMessageComponent {
