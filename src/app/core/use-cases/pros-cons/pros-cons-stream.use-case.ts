791267213a96fbfe783c00dff07dc2d92bcb75e4
jhonpulido1990 <3825@holbertonstudents.com>
2025-02-01 17:13:04 -0500

    respuesta como stream

--- src/app/core/use-cases/pros-cons/pros-cons-stream.use-case.ts
+++ src/app/core/use-cases/pros-cons/pros-cons-stream.use-case.ts
@@ -1,3 +1,36 @@
+import { environment } from "../../../../environments/environment";
+
 export const proConsStreamUseCase = async (prompt: string) => {
-  console.log('proConsStreamUseCase');
+  try {
+    const resp = await fetch(`${environment.backendApi}/pro-cons-discusser-stream`, {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ prompt }),
+    });
+
+    if (!resp.ok) throw new Error('No se pudo realizar la conexion');
+
+    const reader = resp.body?.getReader();
+    if(!reader) {
+      console.log('No se pudo generar el reader');
+      throw new Error('No se pudo generar el reader');
+    }
+
+    const decoder = new TextDecoder();
+    let text = '';
+    while( true ) {
+      const { value, done } = await reader.read();
+      if ( done ) break;
+
+      const decodeChunk = decoder.decode(  value, { stream: true } );
+      text += decodeChunk;
+      console.log(text)
+    }
+
+    return null;
+  } catch (error) {
+    return null;
+  }
 };
