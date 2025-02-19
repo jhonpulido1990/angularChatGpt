6ffdd5d1509c5fe9c687f38da97607c8f8bb042b
jhonpulido1990 <3825@holbertonstudents.com>
2025-02-18 22:54:16 -0500

    marquetacion de la pagina de asistente

--- src/app/presentation/pages/assistant-page/assistant-page.component.ts
+++ src/app/presentation/pages/assistant-page/assistant-page.component.ts
@@ -1,13 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Message } from '../../../interfaces';
+import { ChatMessageComponent } from '../../components/chat-bubbles/chat-message/chat-message.component';
+import { MyMessageComponent } from '../../components/chat-bubbles/my-message/my-message.component';
+import { TextMessageBoxComponent } from '../../components/text-boxes/text-message-box/text-message-box.component';
+import { TypingLoaderComponent } from '../../components/typing-loader/typing-loader.component';
+import { OpenAiService } from '../../services/openai.service';
 
 @Component({
-    selector: 'app-assistant-page',
-    imports: [CommonModule],
-    templateUrl: './assistant-page.component.html',
-    styleUrl: './assistant-page.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-assistant-page',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],
+  templateUrl: './assistant-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AssistantPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
 
+  handleMessage(prompt: string) {
+    console.log(prompt);
+  }
 }
